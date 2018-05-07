a4f3b2a8e23c3a375df29ed3a8b6f15759844e40
Brandon Fosdick <bfosdick@gmail.com>
2018-05-07 12:13:39 -0700

    Fixed clobbering of const_missing in the DSL module

--- lib/grammar/dsl.rb
+++ lib/grammar/dsl.rb
@@ -15,6 +15,15 @@ module Grammar
 		    #  the purpose of catching any uses of the not-yet-defined recursive grammar-in-progress.
 		    # Any such references are replaced with a Recursion wrapper object
 		    enclosing_module = eval("self", block.binding)
+
+		    # But first, store the original const_missing in order to put it back later
+		    original_const_missing = begin
+			enclosing_module.singleton_method(:const_missing)
+		    rescue NameError
+			nil
+		    end
+
+		    # Now inject the method
 		    enclosing_module.define_singleton_method(:const_missing) do |name|
 			recursion_wrapper ||= Grammar::Recursion.new if name == grammar_name
 		    end
@@ -22,6 +31,13 @@ module Grammar
 
 		subklass = Grammar::Builder.new(klass).evaluate(&block)
 		if grammar_name
+		    # Restore the original const_missing
+		    if original_const_missing
+			enclosing_module.define_singleton_method(:const_missing, original_const_missing)
+		    else
+			enclosing_module.singleton_class.send(:undef_method, :const_missing) rescue nil
+		    end
+
 		    # If the recursion wrapper was generated, then the block must have used it, therefore subklass is recursive
 		    if recursion_wrapper
 			case klass
