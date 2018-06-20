4647fa21d0c116ef9b2cbfe3a41bd09370d1742a
Brandon Fosdick <bfoz@bfoz.net>
2018-06-20 14:05:17 -0700

    Added recursion properties to Recursion

--- lib/grammar/recursion.rb
+++ lib/grammar/recursion.rb
@@ -11,6 +11,16 @@ class Grammar::Recursion
     	nil
     end
 
+    # @return [Bool]	Returns true if the first element is left recursive
+    def left_recursive?
+	self.grammar&.respond_to?(:left_recursive?) and self.grammar&.left_recursive?
+    end
+
+    # @return [Bool]	Returns true if grammar is recursive
+    def recursive?
+    	self.grammar&.respond_to?(:recursive?) and self.grammar&.recursive?
+    end
+
     class << self
 	def with(grammar)
 	    self.new(grammar)
